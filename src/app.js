3bf8f70417fbbb565f8c1df67df931af9a7d9cdf
unknown <impactgraphics98@gmail.com>
2021-12-04 17:16:52 -0500

    set up app for oracle

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define path for express configuration
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -110,6 +111,6 @@ app.get('*', (req, res) => {
 
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up guy')
 })
